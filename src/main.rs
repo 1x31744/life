0aebac487c2e7af1e56c835e62a6f8c89e478163
Koopa <koopahello@gmail.com>
2024-01-03 23:35:10 +0000

    added input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use colored::{Colorize};
-use std::{thread, time};
+use core::f32;
+use std::{thread, time, u128};
 use rand::prelude::*;
+use std::io;
+use text_io;
 
-const GRID_SIZE: (f32, f32) = (118.0, 70.0);
 
 struct GameState {
     grid: Vec<Vec<bool>>,
@@ -11,7 +13,7 @@ struct GameState {
  }
 
 impl GameState{
-    pub fn new() -> Self {
+    pub fn new(GRID_SIZE: (i16, i16)) -> Self {
         GameState {
         //* creates a grid that is a 2D vector with all tiles having a value of false with size of GRID_SIZE.1 (casted to usize)
         // makes a vector 40 tiles wide and each vector has it's own 40 cells
@@ -42,7 +44,7 @@ impl GameState{
         return currentDraw;
     }
 
-    pub fn updt(&mut self){
+    pub fn updt(&mut self, GRID_SIZE: (i16, i16)){
         let mut coords: Vec<(usize, usize)> = vec![]; //stores the co-ordinates of the cells that need to be flipped
 
 
@@ -61,7 +63,7 @@ impl GameState{
                 let neighbours 
                 = self.grid[y][left] as u8 
                 + self.grid[up][left] as u8 
-                + self.grid[up][left] as u8 
+                + self.grid[up][x] as u8 
                 + self.grid[up][right] as u8 
                 + self.grid[y][right] as u8
                 + self.grid[down][right] as u8 
@@ -85,9 +87,46 @@ impl GameState{
 
 
 fn main(){
-    let mut state: GameState = GameState::new();
     let startType: &str = "rand";
-    let chanceForLife: u8 = 50;
+
+    //* SET CONFIGURATION VARIABLES */
+    println!("Enter Grid X Size:");
+    let x_size: i16 = text_io::read!();
+
+    println!("Enter Grid Y Size:");
+    let y_size: i16 = text_io::read!();
+
+    println!("Set Chance For Life: (out of 100)");
+    let chanceForLife: u8 = text_io::read!();
+
+    let GRID_SIZE: (i16, i16) = (x_size, y_size);
+    let mut state: GameState = GameState::new(GRID_SIZE);
+
+    println!("Set Refresh Time In MS:");
+    state.waitTime = text_io::read!();
+
+    let mut current_gen: u128 = 0; 
+
+    println!("input O color: (input any for default green) (color options are pink, blue)");
+    let o_color_setting: String = text_io::read!();
+    let o_color: (u8,u8,u8);
+    match o_color_setting.as_str(){
+        "pink" => o_color = (225, 105, 180),
+        "blue" => o_color = (0, 0, 225),
+        _=> o_color = (0, 225, 0) // default is green
+    }
+
+    println!("input X color: (input any for default red) (color options are pink, blue)");
+    let x_color_setting: String = text_io::read!();
+    let x_color: (u8, u8, u8);
+    match x_color_setting.as_str(){
+        "pink" => x_color = (225, 105, 180),
+        "blue" => x_color = (0, 0, 225),
+        _ => x_color = (225, 0, 0) // default is red
+    }
+
+
+
 
     if startType == "rand"{
         for x in &mut state.grid{
@@ -104,6 +143,8 @@ fn main(){
     }
 
     while state.running{
+        current_gen += 1;
+        println!("Current Generation is Gen: {}", current_gen);
 
         //* draw game */
         let noColorOutput: String = state.draw();
@@ -113,7 +154,7 @@ fn main(){
             if c == 'X'{
                 //println!("we are printing X");
                 if !printingX{
-                    print!("{}", colorOutput.as_str().green());
+                    print!("{}", colorOutput.as_str().truecolor(o_color.0, o_color.1, o_color.2));
                     colorOutput = String::from("");
                 }
                 colorOutput.push_str("X ");
@@ -122,7 +163,7 @@ fn main(){
             else if c == 'O' {
                 //println!("we are printing O");
                 if printingX{
-                    print!("{}", colorOutput.as_str().red());
+                    print!("{}", colorOutput.as_str().truecolor(x_color.0, x_color.1, x_color.2));
                     colorOutput = String::from("");
                 }
                 colorOutput.push_str("O ");
@@ -136,14 +177,14 @@ fn main(){
         //print final piece as not printed in for loop
 
         if colorOutput.chars().nth(0).unwrap() == 'X'{
-            print!("{}", colorOutput.as_str().red());
+            print!("{}", colorOutput.as_str().truecolor(x_color.0, x_color.1, x_color.2));
         }
         else {
-            print!("{}", colorOutput.as_str().green());
+            print!("{}", colorOutput.as_str().truecolor(o_color.0, o_color.1, o_color.2));
         }
         //print!("\n{}", noColorOutput);
 
-        state.updt();
+        state.updt(GRID_SIZE);
         thread::sleep(time::Duration::from_millis(state.waitTime));
         clearscreen::clear();
     }
